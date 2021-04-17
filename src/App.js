7efe064b261a9e81130ee37dc40b6a1816e0b952
Saar Davidson <davidsonsaar@gmail.com>
2021-04-17 16:00:57 +0300

    add online users feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
 import Header from './components/Header';
 import Footer from './components/footer';
 import { HashRouter, Route, Switch } from 'react-router-dom';
@@ -11,73 +12,80 @@ import Login from './Pages/Login/Login';
 import PropTypes from 'prop-types';
 import useToken from './Pages/Login/useToken';
 import EditPost from './Pages/EditPost';
+import serverURL from './server';
+
 import About from './Pages/About';
 import UpLoadPost from './Pages/UploadPost';
 import Register from './Pages/Login/Register';
 
-
-
-
-
 function App() {
   const { token, setToken } = useToken();
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(`${serverURL}`, {
+      withCredentials: true,
+    });
+
+    socket.connect();
+
+    socket.on('online', (data) => {
+      setResponse(data);
+    });
+
+    return socket.disconnect;
+  }, [token]);
 
   if (!token) {
     return (
       <HashRouter>
-      <Switch>
-
-      <Route path="/register">
-            <Register setToken={setToken}/>
+        <Switch>
+          <Route path='/register'>
+            <Register setToken={setToken} />
           </Route>
 
-      <Route path="/">
-      <Login setToken={setToken} />
+          <Route path='/'>
+            <Login setToken={setToken} />
           </Route>
-
-
-    
-
-    </Switch>
-    </HashRouter>
+        </Switch>
+      </HashRouter>
     );
   }
   return (
     <HashRouter>
-      <Header setToken={setToken}/>
+      <Header setToken={setToken} online={response.online?.length} />
 
-      <main className="container align-items-center" >
+      <main className='container align-items-center'>
         <Switch>
-          <Route path="/dashboard">
+          <Route path='/dashboard'>
             <Dashboard />
           </Route>
-          {//todo do react rauter for speacil id
+          {
+            //todo do react router for special id
           }
-          <Route path="/post/:id">
+          <Route path='/post/:id'>
             <PostPage />
           </Route>
 
-          <Route path="/editpost/:id">
+          <Route path='/editpost/:id'>
             <EditPost />
           </Route>
 
-          <Route path="/about">
+          <Route path='/about'>
             <About />
           </Route>
-          <Route path="/uploadpost">
+          <Route path='/uploadpost'>
             <UpLoadPost />
           </Route>
 
-          <Route path="/preferences">
+          <Route path='/preferences'>
             <Preferences />
           </Route>
-        
-          <Route exact path="/">
+
+          <Route exact path='/'>
             <Main />
           </Route>
         </Switch>
-
-
       </main>
 
       <Footer />
@@ -85,8 +93,7 @@ function App() {
   );
 }
 
-
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
+  setToken: PropTypes.func.isRequired,
 };
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,75 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import useToken from "../Pages/Login/useToken";
+import useToken from '../Pages/Login/useToken';
 import Logo from '../Resorces/Logo.png';
 
-export default function Header({setToken}) {
+export default function Header({ setToken, online }) {
   const { token } = useToken();
   return (
-
-    <nav className="navbar navbar-expand-lg navbar-dark sticky-top mb-4">
-      <div className="container">
-
-        <Link className="nav-link" to="/">
-          <img className="img-responsive" src={Logo} alt="logo" style={{height: "50px"}} />
+    <nav className='navbar navbar-expand-lg navbar-dark sticky-top mb-4'>
+      <div className='container'>
+        <Link className='nav-link' to='/'>
+          <img
+            className='img-responsive'
+            src={Logo}
+            alt='logo'
+            style={{ height: '50px' }}
+          />
         </Link>
-
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
+        <div style={{ flex: '1', textAlign: 'center', fontWeight: 'bold' }}>
+          Online users: {online}
+        </div>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarResponsive'
+          aria-controls='navbarResponsive'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
         </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home
+        <div className='collapse navbar-collapse' id='navbarResponsive'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>
+                Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/about'>
+                About
+              </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/uploadpost">New Post</Link>
-            </li>
-
-           {token.user.role === "Admin" &&
-            <li className="nav-item">
-              <Link className="nav-link" to="/Dashboard/Dashboard">DashBoard</Link>
-            </li>
-            }
-            <li className="nav-item">
-           
-              <Link    className="btn-danger" to="/" onClick={(() => { localStorage.clear(); setToken(""); })}> Log out</Link>
-              
+            <li className='nav-item'>
+              <Link className='nav-link' to='/uploadpost'>
+                New Post
+              </Link>
             </li>
 
-
+            {token.user.role === 'Admin' && (
+              <li className='nav-item'>
+                <Link className='nav-link' to='/Dashboard/Dashboard'>
+                  DashBoard
+                </Link>
+              </li>
+            )}
+            <li className='nav-item'>
+              <Link
+                className='btn-danger'
+                to='/'
+                onClick={() => {
+                  localStorage.clear();
+                  setToken('');
+                }}
+              >
+                {' '}
+                Log out
+              </Link>
+            </li>
           </ul>
         </div>
-
       </div>
     </nav>
   );
